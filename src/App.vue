c4ae12344981815d68c28a90738755beddd0fdf1
SunGub <1244214660@qq.com>
2019-05-12 22:54:16 +0800

    项目初始化完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header></m-header>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  components: {
+    MHeader
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus">
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill' // 转义es6中的接口 eg: promise
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
