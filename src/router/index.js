f2d09755ed05670672177d1d5847ea2bfef3de13
“e <18576447586@163.com>
2019-05-13 15:49:03 +0800

    页面骨架开发完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Recommend from 'components/recommend/Recommend'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
 
 Vue.use(Router)
 
@@ -7,7 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Vue
+      redirect: Recommend
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      component: Search
     }
   ]
 })
